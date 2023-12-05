eaef4e3068657130ce9f7151dd6aed288073d69e
bagasjs <nakanoyoichi83@gmail.com>
2023-12-05 21:20:31 +0700

    Day 5: Yay

---
+++ day5/README.md
@@ -0,0 +1,7 @@
+# Day 5
+This day's part 2 is really-really hard. I mean it's actually simple, the part 2 is just adding the amount of data.
+But the reason this one is really hard is you need a fast algorithm. My first implementation was sucks and slow even
+when I'm running it in release mode that my old laptop almost crash. This version is better in release mode It took 
+arround 2-3 minutes. I think it's pretty reasonable since the amount of input is very big, considering the algorithm 
+time complexity is actually O(N^3) theoretically there's no multithreading. Maybe there's a better solution, 
+but I'm happy with this.
--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
 use std::fs;
-use std::collections::HashMap;
 use std::ops::Range;
+
 enum Intersection {
-    None, Equal, Inner, Outer, Left, Right
+    None, Equal, In, Out, Left, Right
 }
 
 trait Intersect {
@@ -13,16 +13,17 @@ trait Intersect {
 impl<T> Intersect for Range<T> 
     where T: PartialOrd
 {
+    // 1..50 50..100
     fn intersect_with(&self, other: &Self) -> Intersection {
         if other.end == self.end && other.start == self.start {
             Intersection::Equal
-        } else if other.end > self.end && other.start < self.start {
-            Intersection::Inner
-        } else if self.end > other.end && self.start < other.start {
-            Intersection::Outer
-        } else if self.end > other.end {
-            Intersection::Right
-        } else if self.start < other.start {
+        } else if other.start < self.start && self.end < other.end {
+            Intersection::In
+        } else if self.start <= other.start && other.end < self.end {
+            Intersection::Out
+        } else if other.start <= self.start && self.start < other.end {
+            Intersection::Left
+        } else if other.start <= self.end && self.end < other.end {
             Intersection::Right
         } else {
             Intersection::None
@@ -30,241 +31,157 @@ impl<T> Intersect for Range<T>
     }
 }
 
-struct RangeList {
-    data: Vec<Range<u64>>
-}
-
-impl RangeList {
-    fn new() -> Self {
-        return Self {
-            data: Vec::with_capacity(32),
-        }
-    }
-
-    fn add(&mut self, range: Range<u64>) {
-        for r in self.data.iter_mut() {
-            match range.intersect_with(r) {
-                Intersection::Equal | Intersection::Inner => return,
-                Intersection::Outer => {
-                    r.start = range.start;
-                    r.end = range.end;
-                    return;
-                },
-                Intersection::Left => {
-                    r.start = range.start;
-                    return;
-                },
-                Intersection::Right => {
-                    r.end = range.end;
-                    return;
-                },
-                Intersection::None => {
-                    continue
-                },
-            }
-        }
-
-        self.data.push(range);
-    }
-}
-
 #[derive(Debug)]
-struct SrcToDst {
-    src_start: u64,
-    dst_start: u64,
-    amount: u64,
+struct SrcDst {
+    src: Range<i64>,
+    dst: Range<i64>,
 }
 
-impl SrcToDst {
-    fn new(src_start: u64, dst_start: u64, amount: u64) -> Self {
-        Self { src_start, dst_start, amount }
+impl SrcDst {
+    fn new(src: Range<i64>, dst: Range<i64>) -> Self {
+        Self { src, dst, }
     }
 
-    fn contains(&self, src: u64) -> bool {
-        return self.src_start <= src && src < self.src_start + self.amount;
+    fn contains(&self, src: i64) -> bool {
+        return self.src.start <= src && src <= self.src.end;
     }
-    
-    fn dst_of(&self, src: u64) -> Option<u64> {
-        if self.contains(src) {        
-            let index = src - self.src_start;
-            return Some(self.dst_start + index);
+
+    fn is_valid(&self) -> bool {
+        return (self.src.end - self.src.start) == (self.dst.end - self.dst.start);
+    }
+
+    fn update_on_intersect(&mut self, other: &SrcDst) -> bool {
+        if !other.is_valid() {
+            return false;
+        }
+
+        let mut intersection_occured = false;
+
+        match self.src.intersect_with(&other.src) {
+            Intersection::None => (),
+            Intersection::Equal | Intersection::In => intersection_occured = true,
+            Intersection::Out => {
+                self.src.start = other.src.start;
+                self.src.end = other.src.end;
+                self.dst.start = other.dst.start;
+                self.dst.end = other.dst.end;
+                intersection_occured = true;
+            },
+
+            Intersection::Left => {
+                self.dst.start = other.dst.start;
+                self.src.start = other.src.start;
+                intersection_occured = true;
+            },
+            Intersection::Right => {
+                self.src.end = other.src.end;
+                self.dst.end = other.dst.end;
+                intersection_occured = true;
+            },
+        }
+
+        // match self.dst.intersect_with(&other.dst) {
+        //     Intersection::None => (),
+        //     Intersection::Equal | Intersection::In => {
+        //         intersection_occured = true;
+        //     }
+        //     Intersection::Out => {
+        //         self.dst.start = other.dst.start;
+        //         self.dst.end = other.dst.end;
+        //         intersection_occured = true;
+        //     },
+
+        //     Intersection::Left => {
+        //         self.dst.start = other.dst.start;
+        //         intersection_occured = true;
+        //     },
+        //     Intersection::Right => {
+        //         self.dst.end = other.dst.end;
+        //         intersection_occured = true;
+        //     },
+        // }
+
+        return intersection_occured;
+    }
+
+    fn dst_of(&self, src: i64) -> Option<i64> {
+        if self.contains(src) {
+            let index = src - self.src.start;
+            return Some(self.dst.start + index);
         } else {
             return None;
         }
     }
 }
 
-impl std::fmt::Display for SrcToDst {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let res = write!(f, "source ({}..{}), dst ({}..{})",
-            self.src_start, self.src_start + self.amount,
-            self.dst_start, self.dst_start + self.amount);
-        return res;
+#[derive(Debug)]
+struct SrcDstList {
+    data: Vec<SrcDst>
+}
+
+impl SrcDstList {
+    fn new() -> Self {
+        Self { data: Vec::with_capacity(32) } 
+    }
+
+    fn add(&mut self, item: SrcDst) {
+        for srcdst in self.data.iter_mut() {
+            if srcdst.update_on_intersect(&item) {
+                return;
+            }
+        }
+        self.data.push(item)
+    }
+
+    fn dst_of(&self, src: i64) -> i64 {
+        for srcdst in self.data.iter() {
+            if let Some(dst) = srcdst.dst_of(src) {
+                return dst;
+            }
+        }
+        return src;
     }
 }
 
 #[derive(Debug)]
 struct Almanac {
-    seed_to_soil: Vec<SrcToDst>,
-    soil_to_fertilizer: Vec<SrcToDst>,
-    fertilizer_to_water: Vec<SrcToDst>,
-    water_to_light: Vec<SrcToDst>,
-    light_to_temperature: Vec<SrcToDst>,
-    temperature_to_humidity: Vec<SrcToDst>,
-    humidity_to_location: Vec<SrcToDst>,
-
-    seed_to_soil_cache: HashMap<u64, u64>,
-    soil_to_fertilizer_cache: HashMap<u64, u64>,
-    fertilizer_to_water_cache: HashMap<u64, u64>,
-    water_to_light_cache: HashMap<u64, u64>,
-    light_to_temperature_cache: HashMap<u64, u64>,
-    temperature_to_humidity_cache: HashMap<u64, u64>,
-    humidity_to_location_cache: HashMap<u64, u64>,
-    seed_to_location_cache: HashMap<u64, u64>,
+    seed_to_soil: SrcDstList,
+    soil_to_fertilizer: SrcDstList,
+    fertilizer_to_water: SrcDstList,
+    water_to_light: SrcDstList,
+    light_to_temperature: SrcDstList,
+    temperature_to_humidity: SrcDstList,
+    humidity_to_location: SrcDstList,
 }
 
 impl Almanac {
     fn new() -> Self {
         Self { 
-            seed_to_soil: Vec::new(),
-            soil_to_fertilizer: Vec::new(),
-            fertilizer_to_water: Vec::new(),
-            water_to_light: Vec::new(),
-            light_to_temperature: Vec::new(),
-            temperature_to_humidity: Vec::new(),
-            humidity_to_location: Vec::new(),
-
-            seed_to_soil_cache: HashMap::with_capacity(50), soil_to_fertilizer_cache: HashMap::with_capacity(50),
-            fertilizer_to_water_cache: HashMap::with_capacity(50),
-            water_to_light_cache: HashMap::with_capacity(50),
-            light_to_temperature_cache: HashMap::with_capacity(50),
-            temperature_to_humidity_cache: HashMap::with_capacity(50),
-            humidity_to_location_cache: HashMap::with_capacity(50),
-            seed_to_location_cache: HashMap::with_capacity(50),
+            seed_to_soil: SrcDstList::new(),
+            soil_to_fertilizer: SrcDstList::new(),
+            fertilizer_to_water: SrcDstList::new(),
+            water_to_light: SrcDstList::new(),
+            light_to_temperature: SrcDstList::new(),
+            temperature_to_humidity: SrcDstList::new(),
+            humidity_to_location: SrcDstList::new(),
         }
     }
 
-    fn soil_of(&mut self, seed: u64) -> u64 {
-        if self.seed_to_soil_cache.contains_key(&seed) {
-            return self.seed_to_soil_cache[&seed];
-        } else {
-            for seed_to_soil in self.seed_to_soil.iter() {
-                if let Some(dst) = seed_to_soil.dst_of(seed) {
-                    self.seed_to_soil_cache.insert(seed, dst);
-                    return dst;
-                }
-            }
-            return seed;
-        }
+    fn location_of_seed(&mut self, seed: i64) -> i64 {
+        let mut location = seed;
+        location = self.seed_to_soil.dst_of(location);
+        location = self.soil_to_fertilizer.dst_of(location);
+        location = self.fertilizer_to_water.dst_of(location);
+        location = self.water_to_light.dst_of(location);
+        location = self.light_to_temperature.dst_of(location);
+        location = self.temperature_to_humidity.dst_of(location);
+        location = self.humidity_to_location.dst_of(location);
+        return location;
     }
 
-    fn fertilizer_of(&mut self, soil: u64) -> u64 {
-        if self.soil_to_fertilizer_cache.contains_key(&soil) {
-            return self.soil_to_fertilizer_cache[&soil];
-        } else {
-            for soil_to_fertilizer in self.soil_to_fertilizer.iter() {
-                if let Some(dst) = soil_to_fertilizer.dst_of(soil) {
-                    self.soil_to_fertilizer_cache.insert(soil, dst);
-                    return dst;
-                }
-            }
-            return soil;
-        }
-    }
-
-    fn water_of(&mut self, fertilizer: u64) -> u64 {
-        if self.fertilizer_to_water_cache.contains_key(&fertilizer) {
-            return self.fertilizer_to_water_cache[&fertilizer];
-        } else {
-            for fertilizer_to_water in self.fertilizer_to_water.iter() {
-                if let Some(dst) = fertilizer_to_water.dst_of(fertilizer) {
-                    self.fertilizer_to_water_cache.insert(fertilizer, dst);
-                    return dst;
-                }
-            }
-            return fertilizer;
-        }
-    }
-
-    fn light_of(&mut self, water: u64) -> u64 {
-        if self.water_to_light_cache.contains_key(&water) {
-            return self.water_to_light_cache[&water];
-        } else {
-            for water_to_light in self.water_to_light.iter() {
-                if let Some(dst) = water_to_light.dst_of(water) {
-                    self.water_to_light_cache.insert(water, dst);
-                    return dst;
-                }
-            }
-            return water;
-        }
-    }
-
-    fn temperature_of(&mut self, light: u64) -> u64 {
-        if self.light_to_temperature_cache.contains_key(&light) {
-            return self.light_to_temperature_cache[&light];
-        } else {
-            if let Some(temp) = self.light_to_temperature.iter().find(|st| st.contains(light)) {
-            }
-            for light_to_temperature in self.light_to_temperature.iter() {
-                if let Some(dst) = light_to_temperature.dst_of(light) {
-                    self.light_to_temperature_cache.insert(light, dst);
-                    return dst;
-                }
-            }
-            return light;
-        }
-    }
-
-    fn humidity_of(&mut self, temperature: u64) -> u64 {
-        if self.temperature_to_humidity_cache.contains_key(&temperature) {
-            return self.temperature_to_humidity_cache[&temperature];
-        } else {
-            for temperature_to_humidity in self.temperature_to_humidity.iter() {
-                if let Some(dst) = temperature_to_humidity.dst_of(temperature) {
-                    self.temperature_to_humidity_cache.insert(temperature, dst);
-                    return dst;
-                }
-            }
-            return temperature;
-        }
-    }
-
-    fn location_of(&mut self, humidity: u64) -> u64 {
-        if self.humidity_to_location_cache.contains_key(&humidity) {
-            return self.humidity_to_location_cache[&humidity];
-        } else {
-            for humidity_to_location in self.humidity_to_location.iter() {
-                if let Some(dst) = humidity_to_location.dst_of(humidity) {
-                    self.humidity_to_location_cache.insert(humidity, dst);
-                    return dst;
-                }
-            }
-            return humidity;
-        }
-    }
-
-    fn location_of_seed(&mut self, seed: u64) -> u64 {
-        if self.seed_to_location_cache.contains_key(&seed) {
-            println!("Duplication");
-            return self.seed_to_location_cache[&seed];
-        } else {
-            let mut dst = self.soil_of(seed);
-            dst = self.fertilizer_of(dst);
-            dst = self.water_of(dst);
-            dst = self.light_of(dst);
-            dst = self.temperature_of(dst);
-            dst = self.humidity_of(dst);
-            dst = self.location_of(dst);
-            self.seed_to_location_cache.insert(seed, dst);
-            return dst;
-        }
-    }
-
-    fn find_lowest_location_from_seed_range(&mut self, range: std::ops::Range<u64>) -> (u64, u64) {
-        println!("{range:?}");
-        let mut result_seed = 0u64;
-        let mut result_location = u64::MAX;
+    fn find_lowest_location_from_seed_range(&mut self, range: Range<i64>) -> (i64, i64) {
+        let mut result_seed = 0i64;
+        let mut result_location = i64::MAX;
         for seed in range {
             let location = self.location_of_seed(seed);
             if location < result_location {
@@ -277,10 +194,10 @@ impl Almanac {
     }
 }
 
-fn solve_file(file_path: &str) -> Result<(u64, u64), String> {
+fn solve_file(file_path: &str) -> Result<(i64, i64), String> {
     let file_content = fs::read_to_string(file_path).map_err(|err| err.to_string())?;
     
-    let mut seeds = Vec::<u64>::new();
+    let mut seeds = Vec::<i64>::new();
     let mut almanac = Almanac::new();
 
     let file_content = file_content.lines()
@@ -297,7 +214,7 @@ fn solve_file(file_path: &str) -> Result<(u64, u64), String> {
         if content == "seeds" || content.ends_with("map") {
             label = Some(content);
         } else {
-            let mut numbers: Vec<u64> = Vec::new();
+            let mut numbers: Vec<i64> = Vec::new();
             for num in content.trim().split_whitespace() {
                 numbers.push(num.parse().map_err(|_| "Failed to parse number")?);
             }
@@ -306,16 +223,20 @@ fn solve_file(file_path: &str) -> Result<(u64, u64), String> {
                 if label == "seeds" {
                     seeds.extend(numbers);
                 } else if numbers.len() == 3 {
-                    let src_to_dst = SrcToDst::new(numbers[1], numbers[0], numbers[2]);
+                    let dst = numbers[0];
+                    let src = numbers[1];
+                    let size = numbers[2] - 1;
+                    let src_dst = SrcDst::new(src..src+size, dst..dst+size);
+
                     match label {
-                        "seed-to-soil map" => almanac.seed_to_soil.push(src_to_dst),
-                        "soil-to-fertilizer map" => almanac.soil_to_fertilizer.push(src_to_dst),
-                        "fertilizer-to-water map" => almanac.fertilizer_to_water.push(src_to_dst),
-                        "water-to-light map" => almanac.water_to_light.push(src_to_dst),
-                        "light-to-temperature map" => almanac.light_to_temperature.push(src_to_dst),
-                        "temperature-to-humidity map" => almanac.temperature_to_humidity.push(src_to_dst),
-                        "humidity-to-location map" => almanac.humidity_to_location.push(src_to_dst),
-                        _ => (),
+                        "seed-to-soil map" => almanac.seed_to_soil.add(src_dst),
+                        "soil-to-fertilizer map" => almanac.soil_to_fertilizer.add(src_dst),
+                        "fertilizer-to-water map" => almanac.fertilizer_to_water.add(src_dst),
+                        "water-to-light map" => almanac.water_to_light.add(src_dst),
+                        "light-to-temperature map" => almanac.light_to_temperature.add(src_dst),
+                        "temperature-to-humidity map" => almanac.temperature_to_humidity.add(src_dst),
+                        "humidity-to-location map" => almanac.humidity_to_location.add(src_dst),
+                        _ => return Err("WTF".to_string()),
                     }
                 } else {
                     return Err("Unparseable string".to_string());
@@ -324,9 +245,8 @@ fn solve_file(file_path: &str) -> Result<(u64, u64), String> {
         }
     }
 
-    println!("{:?}", almanac);
     // part 1
-    let mut part_1_result = u64::MAX;
+    let mut part_1_result = i64::MAX;
     let mut part_1_seed = 0;
     for seed in seeds.iter() {
         let seed = seed.clone();
@@ -338,17 +258,17 @@ fn solve_file(file_path: &str) -> Result<(u64, u64), String> {
     }
 
     // part 2
-    let mut part_2_result = u64::MAX;
+    let mut part_2_result = i64::MAX;
     let mut part_2_seed = 0;
     for chunk in seeds.chunks(2) {
         let start = chunk.get(0).ok_or("Failed to get the start of the seed range")?.clone();
         let amount = chunk.get(1).ok_or("Failed to get the start of the seed range")?.clone();
+        println!("{start} {amount}");
         let (seed, location) = almanac.find_lowest_location_from_seed_range(start..start+amount);
         if location < part_2_result {
             part_2_result = location;
             part_2_seed = seed;
         }
-        break;
     }
 
     println!("Seed of part 1: {part_1_seed}");
