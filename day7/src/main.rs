35959c29d37502a25383bb101a32d5829d026ad1
bagasjs <nakanoyoichi83@gmail.com>
2023-12-07 14:42:46 +0700

    Day 7: Part 1

---
+++ day7/src/main.rs
@@ -0,0 +1,151 @@
+use std::env;
+use std::fs;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Card {
+    Unknown,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum HandKind {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAkind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+struct Hand {
+    cards: [Card; 5],
+    kind: HandKind,
+    bid: i32,
+}
+
+impl Hand {
+    fn new(cards: &str, bid: i32) -> Result<Self, String> {
+        if cards.len() != 5 {
+            return Err("Allowed card for a hand is only 5".to_string());
+        }
+
+        let mut result = Self { bid, kind: HandKind::HighCard, cards: [Card::Unknown, Card::Unknown, Card::Unknown, Card::Unknown, Card::Unknown] };
+        let cards: Vec<char> = cards.chars().collect();
+        for i in 0..5 {
+            let current_card = match cards[i] {
+                'A' => Card::Ace,
+                'K' => Card::King,
+                'Q' => Card::Queen,
+                'J' => Card::Jack,
+                'T' => Card::Ten,
+                '9' => Card::Nine,
+                '8' => Card::Eight,
+                '7' => Card::Seven,
+                '6' => Card::Six,
+                '5' => Card::Five,
+                '4' => Card::Four,
+                '3' => Card::Three,
+                '2' => Card::Two,
+                _ => Card::Unknown,
+            };
+
+            let mut same_card_counter = 0;
+            for j in 0..i {
+                if current_card == result.cards[j] {
+                    same_card_counter += 1;
+                }
+            }
+
+            result.cards[i] = current_card;
+            result.kind = match same_card_counter {
+                0 => result.kind,
+                1 => match result.kind {
+                    HandKind::HighCard => HandKind::OnePair,
+                    HandKind::OnePair => HandKind::TwoPair,
+                    HandKind::ThreeOfAkind => HandKind::FullHouse,
+                    _ => return Err("This should not be happened".to_string()),
+                },
+                2 => match result.kind {
+                    HandKind::OnePair => HandKind::ThreeOfAkind,
+                    HandKind::TwoPair => HandKind::FullHouse,
+                    _ => return Err("This should not be happened".to_string()),
+                },
+                3 => match result.kind {
+                    HandKind::ThreeOfAkind => HandKind::FourOfAKind,
+                    _ => return Err("This should not be happened".to_string()),
+                },
+                4 => match result.kind {
+                    HandKind::FourOfAKind => HandKind::FiveOfAKind,
+                    _ => return Err("This should not be happened".to_string()),
+                }
+                _ => return Err("Unreachable error".to_string())
+            };
+        }
+
+        Ok(result)
+    }
+}
+
+fn cmp_hands(a: &Hand, b: &Hand) -> std::cmp::Ordering {
+    match a.kind.cmp(&b.kind) {
+        std::cmp::Ordering::Equal => {
+            for i in 0..a.cards.len() {
+                match a.cards[i].cmp(&b.cards[i]) {
+                    std::cmp::Ordering::Equal => continue,
+                    std::cmp::Ordering::Greater => return std::cmp::Ordering::Greater,
+                    std::cmp::Ordering::Less => return std::cmp::Ordering::Less,
+                }
+            }
+            return std::cmp::Ordering::Equal;
+        },
+        std::cmp::Ordering::Less => std::cmp::Ordering::Less,
+        std::cmp::Ordering::Greater => std::cmp::Ordering::Greater,
+    }
+}
+
+fn solve_file(file_path: &str) -> Result<(i32, i32), String> {
+    let file_content = fs::read_to_string(file_path).map_err(|e| e.to_string())?;
+    let mut hands: Vec<Hand> = Vec::new();
+    for line in file_content.lines() {
+        let mut line_it = line.split_whitespace().into_iter();
+        let cards = line_it.next().ok_or("Failed to parse cards")?;
+        let bid: i32 = line_it.next()
+            .ok_or("Failed to parse the bid")?
+            .parse()
+            .map_err(|_| "Failed to parse bid to number")?;
+        let hand = Hand::new(cards, bid)?;
+        hands.push(hand);
+    }
+    hands.sort_by(|a, b| cmp_hands(a, b));
+    let mut result_of_part_1 = 0;
+    for i in 0..hands.len() {
+        println!("{:?} {:?} {}", hands[i].cards, hands[i].kind, hands[i].bid);
+        result_of_part_1 += hands[i].bid * (i + 1) as i32;
+    }
+    return Ok((result_of_part_1, 1,));
+}
+
+fn main() {
+    let file_path = env::args().skip(1).next().expect("Please provide an input file path");
+
+    match solve_file(&file_path) {
+        Ok((part_1, part_2)) => {
+            println!("Result of part 1: {part_1}");
+            println!("Result of part 2: {part_2}");
+        },
+        Err(error_message) => eprintln!("ERROR: {}", error_message),
+    }
+}
